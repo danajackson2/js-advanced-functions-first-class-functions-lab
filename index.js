e618a82face683a694e263962511a24255270734
danajackson2 <danajackson2@gmail.com>
2021-01-19 13:27:46 -0800

    the matrix

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier){
+    return function(fare){return fare * multiplier}
+}
+
+function fareDoubler(fare){       
+    return createFareMultiplier(2)(fare)
+}
+
+function fareTripler(fare){       
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(arr, func){
+    return func(arr)
+}
